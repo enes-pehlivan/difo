d090e11e8b1f37ab2f391c793543bfa42f1902fa
enes-pehlivan <epehlivan@tyrofox.com>
2022-11-16 14:43:03 +0100

    difo

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h2 className="text-4xl text-center font-extrabold">ENES</h2>
+    </div>
+  );
+};
+
+export default index;
